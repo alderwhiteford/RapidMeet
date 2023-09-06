f1f604d4b5a576e31c634e7c2f6a6c71f789cb9b
Alder Whiteford <alderjwhiteford@gmail.com>
2023-09-05 21:20:33 -0400

    Initial Commit

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyOUa054AYhpSXpdEam6IpfgdFoKuqakY",
+  authDomain: "when3meet-30a61.firebaseapp.com",
+  projectId: "when3meet-30a61",
+  storageBucket: "when3meet-30a61.appspot.com",
+  messagingSenderId: "645581900802",
+  appId: "1:645581900802:web:5cca632205aafc7f0beb1b",
+  measurementId: "G-RFKTL57DEY"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db }
