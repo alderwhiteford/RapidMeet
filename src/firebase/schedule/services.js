5acf97226457051cd20d79b9c79fa863c08ad97e
Alder Whiteford <alderjwhiteford@gmail.com>
2023-09-06 23:33:37 -0400

    Update Firebase APIs

--- src/firebase/schedule/services.js
+++ src/firebase/schedule/services.js
@@ -9,6 +9,7 @@ const scheduleInit = {
   start_time: '',
   end_time: '',
   user_names: [],
+  user_emails: [],
   user_ids: [],
   availability: {}
 }
@@ -17,74 +18,69 @@ export async function createSchedule({ name, start_time, end_time }) {
   const newSchedule = { ...scheduleInit, name, start_time, end_time }
   const newScheduleRef = doc(collection(db, "schedule"));
 
-  await setDoc(newScheduleRef, newSchedule)
-    .then({
-      // Response is undefined.
-      // Trigger success
-    })
-    .catch((error) => {
-      // Need to add error handling feature:
-      console.log(error);
-    });
+  try {
+    await setDoc(newScheduleRef, newSchedule)
+    return newSchedule; // Handle success
+  }
+  catch (error) {
+    return // Throw error
+  }
 }
 
-export async function getScheduleById({ scheduleId, setSchedule }) {
-  onSnapshot(doc(db, "schedule", scheduleId), 
-    (snapshot) => {
-      setSchedule(snapshot.data());
-    },
-    (error) => {
-      // Need to add error handling feature:
-      console.log(error);
+export async function getScheduleById({ scheduleId }) {
+  return new Promise(async (resolve, reject) => {
+    const docRef = doc(db, 'schedule', scheduleId);
+
+    const unsubscribe = onSnapshot(docRef, 
+      (snapshot) => {
+        resolve(snapshot.data());
+      },
+      (error) => {
+        reject({ error, unsubscribe }); // Throw error
     });
+  });
 }
 
-export async function addScheduleUser({ scheduleId, user_name, existing_user_ids }) {
+export async function addScheduleUser({ scheduleId, user_name, user_email, existing_user_ids, existing_user_emails }) {
   const user_id = stringToUniqueNumber(user_name);
+
+  if (existing_user_ids.includes(user_id) || existing_user_emails.includes(user_email)) {
+    return // Throw error
+   }
   
-  if (!existing_user_ids.includes(user_id)) {
-    const scheduleRef = doc(db, "schedule", scheduleId);
+  const scheduleRef = doc(db, "schedule", scheduleId);
+  try {
     await updateDoc(scheduleRef, {
       user_ids: arrayUnion(user_id),
       user_names: arrayUnion(user_name)
     })
-    .then({
-      // Response is undefined.
-      // Trigger success
-    })
-    .catch((error) => {
-      // Need to add error handling feature:
-      console.log(error);
-    });
-  } else {
-    // Need to add error handling feature:
-    console.log("User already exists!");
+    return // Handle success
   }
+  catch (error) {
+    return // Throw error
+  };
 }
 
 export async function updateUserAvailability({ scheduleId, user_id, availability, existing_availability, existing_user_ids }) {
+  if (!existing_user_ids.includes(user_id)) {
+    return // Throw error
+  }
+
   for (let time of availability) {
     if (!existing_availability[time]) {
       existing_availability[time] = [];
     }
     existing_availability[time].push(user_id);
   }
-  
-  if (existing_user_ids.includes(user_id)) {
-    const scheduleRef = doc(db, "schedule", scheduleId);
+
+  const scheduleRef = doc(db, "schedule", scheduleId);
+  try {
     await updateDoc(scheduleRef, {
       availability: existing_availability
-    })
-    .then({
-      // Response is undefined.
-      // Trigger success
-    })
-    .catch((error) => {
-      // Need to add error handling feature:
-      console.log(error);
-    })
-  } else {
-    // Need to add error handling feature:
-    console.log("User doesn't exist!");
+    });
+    return // Handle success
+  } 
+  catch (error) {
+    return // Throw error
   }
 }
