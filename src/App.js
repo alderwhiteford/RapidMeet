81b696790943152e055885a56eb4d63fdb8df805
Alder Whiteford <alderjwhiteford@gmail.com>
2023-09-15 00:43:10 -0400

    Remove MUI Localization Provider (For Now)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import CreateSchedule from './views/CreateSchedule';
 import Schedule from './views/Schedule';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 function App() {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Routes>
-        <Route path="/" element={<CreateSchedule />} />
-        <Route path="/:scheduleId" element={<Schedule />} />
-      </Routes>
-    </LocalizationProvider>
+    <Routes>
+      <Route path="/" element={<CreateSchedule />} />
+      <Route path="/:scheduleId" element={<Schedule />} />
+    </Routes>
   );
 }
 
