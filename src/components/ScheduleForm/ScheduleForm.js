77d3f72c63bf8b7f2e72c8146e5adaf54f5a49c5
Ethan Johnson <ethanpv34@gmail.com>
2023-09-18 21:48:09 -0400

    Moves createSchedule logic to form, adds timeOptions array constant, adds dropdowns with dynamic enabling and errors

--- src/components/ScheduleForm/ScheduleForm.js
+++ src/components/ScheduleForm/ScheduleForm.js
@@ -1,95 +1,140 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
-import { Button, TextField } from '@mui/material';
-import { useForm } from 'react-hook-form';
+import { Button, InputLabel, FormControl, FormHelperText, MenuItem, Select, TextField } from '@mui/material';
+import { Controller, useForm } from 'react-hook-form';
 
-export default function ScheduleForm({ onSubmitHandler }) {
+import { createSchedule } from '../../services/scheduleApi';
+import { timeOptions } from '../../utils/constants';
+
+export default function ScheduleForm() {
+  const [startTime, setStartTime] = useState('Select start time');
+  const [endTime, setEndTime] = useState('Select end time');
+
+  const navigate = useNavigate();
   const {
+    control, 
     register,
     handleSubmit,
     formState: { errors }
   } = useForm({
         defaultValues: {
           schedule_name: '',
-          start_time: '',
-          end_time: '',
         }
       }
   );
 
-  const SubmitButton = styled(Button)({
-  height: '40px',
-  backgroundColor: '#D3D3D3',
-  boxShadow: 'none',
-  textTransform: 'none',
+  const handleCreateSchedule = ({ schedule_name }) => {
+    createSchedule({
+      name: schedule_name,
+      start_time: startTime,
+      end_time: endTime,
+    })
+      .then((res) => {
+        navigate(`/${res.data.id}`);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
-  '&:hover': {
-    backgroundColor: '#04a43c',
+  const SubmitButton = styled(Button)({
+    height: '40px',
+    backgroundColor: '#D3D3D3',
     boxShadow: 'none',
-  },
+    textTransform: 'none',
+
+    '&:hover': {
+      backgroundColor: '#04a43c',
+      boxShadow: 'none',
+    },
   });
 
   const StyledForm = styled('form')({
-  display: 'flex',
-  flexDirection: 'column'
-  })
+    display: 'flex',
+    flexDirection: 'column',
+  });
 
   const StyledTextField = styled(TextField)({
-  marginBottom: '15px',
-  })
+    marginBottom: '15px',
+  });
 
   return (
-    <StyledForm onSubmit={handleSubmit(onSubmitHandler)}>
-      <StyledTextField 
-        error={!!errors.name}
-        helperText={errors.name?.message ?? " "}
+    <StyledForm onSubmit={handleSubmit(handleCreateSchedule)}>
+      <StyledTextField
+        error={!!errors.schedule_name}
+        helperText={errors.schedule_name?.message ?? ' '}
         InputLabelProps={{ shrink: true }}
         label="Schedule Name"
-        {...register(
-          'name',
-          { required: "Please provide a name" }
-        )}
+        {...register('schedule_name', { required: 'Please provide a name' })}
         type="text"
         variant="outlined"
       />
-      <StyledTextField 
-        error={!!errors.start_time}
-        helperText={errors.start_time?.message ?? " "}
-        InputLabelProps={{ shrink: true }}
-        label="Start Time"
-        {...register(
-          'start_time',
-          { required: "Please provide a start time" }
-        )}
-        type="text"
-        variant="outlined"
-      />
-      <StyledTextField 
-        error={!!errors.end_time}
-        helperText={errors.end_time?.message ?? " "}
-        InputLabelProps={{ shrink: true }}
-        label="End Time"
-        {...register(
-          'end_time',
-          { required: "Please provide an end time" }
-        )}
-        type="text"
-        variant="outlined"
-      />
-      <StyledTextField 
-        error={!!errors.dates}
-        helperText={errors.dates?.message ?? " "}
-        InputLabelProps={{ shrink: true }}
-        label="Dates"
-        {...register(
-          'text',
-          { required: "Please provide at least one date" }
-        )}
-        type="date"
-        variant="outlined"
-      />
-      <SubmitButton 
-        variant="contained" 
-        type="submit" 
+      <FormControl variant="outlined" style={{ marginBottom: '15px' }}>
+        <InputLabel>Start Time</InputLabel>
+        <Controller
+          name="start_time"
+          control={control}
+          defaultValue=""
+          rules={{ required: 'Please provide a start time' }}
+          render={({ field }) => (
+            <Select
+              {...field}
+              label="Start Time"
+              value={startTime}
+              onChange={(e) => {
+                setStartTime(e.target.value);
+                field.onChange(e);
+              }}
+            >
+              {timeOptions.map((time) => (
+                <MenuItem key={time} value={time}>
+                  {time}
+                </MenuItem>
+              ))}
+            </Select>
+          )}
+        />
+        <FormHelperText error={!!errors.start_time}>
+          {errors.start_time?.message}
+        </FormHelperText>
+      </FormControl>
+
+      <FormControl variant="outlined" style={{ marginBottom: '15px' }}>
+        <InputLabel>End Time</InputLabel>
+        <Controller
+          name="end_time"
+          control={control}
+          defaultValue=""
+          rules={{ required: 'Please provide an end time' }}
+          render={({ field }) => (
+            <Select
+              {...field}
+              label="End Time"
+              value={endTime}
+              onChange={(e) => {
+                setEndTime(e.target.value);
+                field.onChange(e);
+              }}
+              disabled={startTime === 'Select start time'}
+            >
+              {timeOptions
+                .slice(startTime ? timeOptions.indexOf(startTime) + 1 : 0)
+                .map((time) => (
+                  <MenuItem key={time} value={time}>
+                    {time}
+                  </MenuItem>
+                ))}
+            </Select>
+          )}
+        />
+        <FormHelperText error={!!errors.end_time}>
+          {errors.end_time?.message}
+        </FormHelperText>
+      </FormControl>
+      <SubmitButton
+        variant="contained"
+        type="submit"
       >
         Create Schedule
       </SubmitButton>
