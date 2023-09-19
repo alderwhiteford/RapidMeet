b3c3b0004401f624b3c6cf25082374efeffae995
Alder Whiteford <alderjwhiteford@gmail.com>
2023-09-19 16:03:44 -0400

    Update Firebase Config (#18)

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDyOUa054AYhpSXpdEam6IpfgdFoKuqakY",
-  authDomain: "when3meet-30a61.firebaseapp.com",
-  projectId: "when3meet-30a61",
-  storageBucket: "when3meet-30a61.appspot.com",
-  messagingSenderId: "645581900802",
-  appId: "1:645581900802:web:5cca632205aafc7f0beb1b",
-  measurementId: "G-RFKTL57DEY"
+  apiKey: "AIzaSyAArC9SqsIN0xluQ4V59V_itbW95WomNx0",
+  authDomain: "scheduler-c7a0b.firebaseapp.com",
+  projectId: "scheduler-c7a0b",
+  storageBucket: "scheduler-c7a0b.appspot.com",
+  messagingSenderId: "230637557804",
+  appId: "1:230637557804:web:517d9e7f63e6dc6b59d989"
 };
 
 const app = initializeApp(firebaseConfig);
