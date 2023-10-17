4b16986afd6ad550770b9b99333d0e4ed21241fe
Alder Whiteford <alderjwhiteford@gmail.com>
2023-10-17 16:36:31 -0400

    Schedule First Attempt

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -32,3 +32,28 @@ export const timeOptions = [
   '10:00 pm',
   '11:00 pm',
 ];
+
+export const months = {
+  0: 'Jan',
+  1: 'Feb',
+  2: 'Mar',
+  3: 'Apr',
+  4: 'May',
+  5: 'Jun',
+  6: 'Jul',
+  7: 'Aug',
+  8: 'Sep',
+  9: 'Oct',
+  10: 'Nov',
+  11: 'Dec',
+}
+
+export const days = {
+  0: 'Sun',
+  1: 'Mon',
+  2: 'Tue',
+  3: 'Wed',
+  4: 'Thu',
+  5: 'Fri',
+  6: 'Sat',
+}
