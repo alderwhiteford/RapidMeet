a75f70c0f6b5aac21d1f2d3a72c490809a1fc1f9
Ethan Johnson <ethanpv34@gmail.com>
2024-01-19 14:30:37 -0500

    Adds 404 page. Kittyyyyy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import CreateSchedule from './views/CreateSchedule';
 import Schedule from './views/Schedule';
+import PageNotFound from './views/PageNotFound';
 import { PrimeReactProvider } from 'primereact/api';
 import "primereact/resources/themes/saga-green/theme.css";
 import "primereact/resources/primereact.min.css";  
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<CreateSchedule />} />
         <Route path="/:scheduleId" element={<Schedule />} />
+        <Route path="/page-not-found" element={<PageNotFound />} />
       </Routes>
     </PrimeReactProvider>
   );
