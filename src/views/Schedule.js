d0412a11284064f789187c7383781ca42e4df344
Ethan Johnson <97696910+ethanpv34@users.noreply.github.com>
2024-01-19 19:38:03 -0500

    Final changes (#41)
    * First commit, WIP
    
    * Second commit, WIP
    
    * Made a lot of changes i dont even remember lol
    
    * More changes
    
    * more stuff
    
    * Woah even more changes
    
    * More changes, refactored some stuff, WIP
    
    * More stufffff
    
    * Fixed merge conflicts, added users to req attendees map, snackbar for share link
    
    * Made some changes and updates
    
    * Adds minwidth to calendar on mobile to make it bigger

--- src/views/Schedule.js
+++ src/views/Schedule.js
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
